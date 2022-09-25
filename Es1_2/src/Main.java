d1fa1b7f68f041d63fe3fdefd24d1da52dc538e4
andreasvevamonteamaro <monteamaroandrea@me.com>
2022-09-25 21:01:05 +0200

    Add project

--- Es1_2/src/Main.java
+++ Es1_2/src/Main.java
@@ -17,10 +17,10 @@
  */
 
 import java.io.*;
-import java.util.*;
-import java.text.*;
+import java.text.DecimalFormat;
+import java.util.StringTokenizer;
 
-class Ssq1Sum {                                 /* sum of ...           */
+class Ssq2Sum {                                 /* sum of ...           */
     double delay;                                 /*   delay times        */
     double wait;                                  /*   wait times         */
     double service;                               /*   service times      */
@@ -40,8 +40,8 @@ class Ssq1Sum {                                 /* sum of ...           */
     }
 }
 
-class Ssq1 {
-    static String FILENAME = "/Users/andreasvevamonteamaro/Downloads/ssq1.dat";          /* input data file */
+class Ssq2 {
+    static String FILENAME = "/Users/andreasvevamonteamaro/Downloads/ssq2.dat";          /* input data file */
     static double START = 0.0;
 
     public static void main(String[] args) throws IOException {
@@ -60,7 +60,10 @@ class Ssq1 {
             String line = null;
             StringTokenizer st = null;
             long   index     = 0;                     /* job index            */
-            double arrival   = START;                 /* arrival time         */
+            //double arrival   = START;                 /* arrival time         */
+            double arrival=0.0;
+            double intarrival =START;
+            double oldarrival=0.0;
             double delay;                             /* delay in queue       */
             double service;                           /* service time         */
             double wait;                              /* delay + service      */
@@ -81,14 +84,18 @@ class Ssq1 {
             double time=400.0;                        /* time                 */
             int jobInServerNode=0;                    /*number of jobs in the service node at a specified time */
 
-            Ssq1Sum sum = new Ssq1Sum();
+            Ssq2Sum sum = new Ssq2Sum();
             sum.initSumParas();
 
             while ( (line = in.readLine()) != null ) {
                 index++;
+
                 st = new StringTokenizer(line);
 
-                arrival = Double.parseDouble(st.nextToken());
+                intarrival = Double.parseDouble(st.nextToken());
+
+                arrival= intarrival + oldarrival;
+                System.out.println(arrival);
                 if (arrival < departure)
                     delay    = departure - arrival;       /* delay in queue    */
                 else
@@ -110,7 +117,7 @@ class Ssq1 {
                 if(arrival<time & time<departure)
                     jobInServerNode++;
 
-
+            oldarrival=arrival;
             }
             sum.interarrival = arrival - START;
 
@@ -124,7 +131,7 @@ class Ssq1 {
             avg_n_in_service = sum.service/total_completation_time;
 
             DecimalFormat f = new DecimalFormat("###0.00");
-
+            System.out.println(" Modified version");
             System.out.println("\nfor " + index + " jobs");
             System.out.println("   average interarrival time =  " + f.format(sum.interarrival / index));
             System.out.println("   average service time .... =  " + f.format(sum.service / index));
@@ -145,14 +152,8 @@ class Ssq1 {
             System.out.println("   the proportion of customer that were delayed ="+ f.format((double) numOfDelay/index));
 
 
-
-           // System.out.println("   last arrival ...............=  " + f.format(arrival));
-            // System.out.println("   max delay ................=  " + f.format(maxDelay));
-           // System.out.println("   last service time ..........=  " + f.format(lastservice));
-           // System.out.println("   total completation time ....=  " + f.format(arrival+sum.delay+lastservice));
-
         } catch (EOFException eofe) {
-            System.out.println("Ssq1:" + eofe);
+            System.out.println("Ssq2:" + eofe);
         }
         // if the file opened okay, make sure we close it
         fis.close();
